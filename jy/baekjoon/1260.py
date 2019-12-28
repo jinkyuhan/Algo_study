ac76bd74a93207fcfd193c38e88423cd383c57d9
cheesecat47 <cheesecat47@gmail.com>
2019-12-28 22:16:18 +0900

    Baekjoon #1260 try2
    Runtime error occured.

--- jy/baekjoon/1260.py
+++ jy/baekjoon/1260.py
@@ -3,11 +3,9 @@ graph = {}
 
 for i in range(m):
     v1, v2 = map(int, input().split())
-    
     if v1 not in graph:
         graph[v1] = []
     graph[v1].append(v2)
-    
     if v2 not in graph:
         graph[v2] = []
     graph[v2].append(v1)
@@ -19,13 +17,13 @@ dfs = []
 while len(dfs) < len(graph):
     this = stack.pop()
 
+    if this not in dfs:
+        dfs.append(this)
+
     for i in sorted(graph[this], reverse=True):
         if i not in dfs:
             stack.append(i)
-
-    if this not in dfs:
-        dfs.append(this)
-print(dfs)
+print(' '.join([str(i) for i in dfs]))
 
 #bfs
 import queue
@@ -35,11 +33,11 @@ bfs = []
 
 while len(bfs) < len(graph):
     this = q.get()
+    
+    if this not in bfs:
+        bfs.append(this)
 
     for i in sorted(graph[this]):
         if i not in bfs:
             q.put(i)
-    
-    if this not in bfs:
-        bfs.append(this)
-print(bfs)
+print(' '.join([str(i) for i in bfs]))
