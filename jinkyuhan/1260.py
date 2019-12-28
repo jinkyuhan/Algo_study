a18c12ca0edf42d790323a029be996f67d417957
jinkyuhan <gkswlsrb95@gmail.com>
2019-12-28 23:16:36 +0900

    Fix 1260

--- jinkyuhan/1260.py
+++ jinkyuhan/1260.py
@@ -5,24 +5,21 @@ WHITE = 16
 GRAY = 32
 
 N, M, START = list(map(int, input().split()))
-# sorting input
-edges = [list(map(int, input().split())) for _ in range(M)]
-edges = sorted(edges, key=lambda edge: (edge[0], edge[1]))
 
 graph = [[] for _ in range(N + 1)]
 color = [WHITE for _ in range(N + 1)]
 
-for e in edges:
-    from_vertex = e[0]
-    to_vertex = e[1]
-    graph[from_vertex].append({
-        'to_vertex': to_vertex,
-        'weight': 0
-    })
-    graph[to_vertex].append({
-        'to_vertex': from_vertex,
-        'weight': 0
-    })
+# make adjacency list
+for _ in range(M):
+    from_vertex, to_vertex = list(map(int, input().split()))
+graph[from_vertex].append({
+    "to_vertex": to_vertex,
+    "weight": 0
+})
+graph[to_vertex].append({
+    "to_vertex": from_vertex,
+    "weight": 0
+})
 
 
 # DFS
@@ -31,8 +28,8 @@ def dfs(start):
     print(start, end=' ')
     color[start] = BLACK  # visit mark
 
-    # recursion about all edges
-    for u in graph[start]:
+    # recursion about all sorted edges
+    for u in sorted(graph[start], key=lambda edge: edge["to_vertex"]):  # sorted by to_vertex
         v = u["to_vertex"]
         if color[v] == WHITE:
             dfs(v)
@@ -40,7 +37,6 @@ def dfs(start):
 
 # BFS
 def bfs(start):
-
     # enqueue root
     q = deque()
     q.append(start)
@@ -52,7 +48,7 @@ def bfs(start):
         color[u] = BLACK
         print(u, end=' ')
         # iterate about all edges
-        for each_vertex in graph[u]:
+        for each_vertex in sorted(graph[u], key=lambda edge: edge["to_vertex"]):  # sorted by to_vertex
             v = each_vertex["to_vertex"]
             if color[v] == WHITE:
                 color[v] = GRAY
