c8e7926d6957d134329a50d5a34957ee34952134
cheesecat47 <cheesecat47@gmail.com>
2019-12-31 21:22:56 +0900

    Baekjoon #1260 - No runtime err, but wrong

--- jy/baekjoon/1260.py
+++ jy/baekjoon/1260.py
@@ -1,39 +1,60 @@
-n, m, v = map(int, input().split())
 graph = {}
+visited = []
+dfs_result = []
+bfs_result = []
 
-for i in range(m):
-    v1, v2 = map(int, input().split())
-    if v1 not in graph:
-        graph[v1] = []
-    graph[v1].append(v2)
-    if v2 not in graph:
-        graph[v2] = []
-    graph[v2].append(v1)
 
-#dfs
-stack = [v]
-dfs = []
+def dfs(u):
+    stack = [u]
 
-while stack:
-    this = stack.pop()
+    while len(stack) > 0:
+        this = stack.pop()
+        dfs_result.append(this)
+        visited[this] = 1
 
-    if this not in dfs:
-        dfs.append(this)
+        for i in sorted(graph[this], reverse=True):
+            if visited[i] == -1:
+                visited[i] = 1
+                stack.append(i)
 
-    for i in sorted(graph[this], reverse=True):
-        if i not in dfs:
-            stack.append(i)
-print(' '.join([str(i) for i in dfs]))
 
-#bfs
-q = [v]
-bfs = [v]
+def bfs(u):
+    from collections import deque
+    q = deque()
+    q.appendleft(u)
 
-while q:
-    this = q.pop(0)
+    while len(q) > 0:
+        this = q.pop()
+        bfs_result.append(this)
+        visited[this] = 1
 
-    for i in sorted(graph[this]):
-        if i not in bfs:
-            q.append(i)
-            bfs.append(i)
-print(' '.join([str(i) for i in bfs]))
+        for i in sorted(graph[this]):
+            if visited[i] == -1:
+                visited[i] = 1
+                q.appendleft(i)
+
+
+if __name__ == "__main__":
+    n, m, v = map(int, input().split())
+
+    for i in range(m):
+        v1, v2 = map(int, input().split())
+        if v1 not in graph:
+            graph[v1] = []
+        graph[v1].append(v2)
+        if v2 not in graph:
+            graph[v2] = []
+        graph[v2].append(v1)
+
+    for _ in range(n+1):
+        visited.append(-1)
+
+    dfs(v)
+    print(' '.join([str(i) for i in dfs_result]))
+
+    visited.clear()
+    for _ in range(n+1):
+        visited.append(-1)
+
+    bfs(v)
+    print(' '.join([str(i) for i in bfs_result]))
