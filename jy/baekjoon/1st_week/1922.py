17c107ba736c5e1b44414489d41c6f95dc913394
cheesecat47 <cheesecat47@gmail.com>
2020-01-05 23:33:45 +0900

    Baekjoon #1922 MST - Prim's algorithm
    1st try - using taegeon's code

---
+++ jy/baekjoon/1st_week/1922.py
@@ -0,0 +1,41 @@
+import heapq
+
+
+def prim(s):
+    sum = 0
+    q = []
+    visited = [0 for _ in range(n)]
+
+    visited[s-1] = 1
+    for u in adj[s]:
+        heapq.heappush(q, u)
+
+    while q:
+        wi, vi = heapq.heappop(q)
+        # print('wi, vi', wi, vi)
+        if visited[vi-1] == 0:
+            visited[vi-1] = 1
+            sum += wi
+            for u in adj[vi]:
+                heapq.heappush(q, u)
+        
+    return sum
+
+
+if __name__ == "__main__":
+    n = int(input())
+    m = int(input())
+
+    # build an adjacency list
+    adj = [[] for _ in range(n+1)]
+    for _ in range(m):
+        v1, v2, w = map(int, input().split())
+        adj[v1].append([w, v2])
+        adj[v2].append([w, v1])
+
+    # print('adj')
+    for i in adj:
+        print(i)
+
+    # Prim's algorithm
+    print(prim(1))
