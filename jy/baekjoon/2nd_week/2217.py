b1f8b7f815927c12fd4dc8a8fbd33b9d74faf675
cheesecat47 <cheesecat47@gmail.com>
2020-01-07 21:19:13 +0900

    #2217 - solving process

--- jy/baekjoon/2nd_week/2217.py
+++ jy/baekjoon/2nd_week/2217.py
@@ -1,20 +1,10 @@
 import sys
-
-if __name__ == "__main__":
-    n = int(sys.stdin.readline())
-    rope = [int(sys.stdin.readline()) for _ in range(n)]
-
-    rope_asc = sorted(rope)
-    this_idx = 0    # x th rope -> n-x ropes are stronger
-    max_weight = 0
-
-    while this_idx < n:
-        this_weight = rope_asc[this_idx]
-        k = n - this_idx
-        wi = this_weight * k
-
-        if max_weight < wi:
-            max_weight = wi
-        this_idx += 1
-
-    print(max_weight)
+n = int(sys.stdin.readline())
+a = sorted([int(sys.stdin.readline()) for _ in range(n)])
+i = m = 0
+while i < n:
+    w = a[i] * (n - i)
+    if m < w:
+        m = w
+    i += 1
+print(m)
