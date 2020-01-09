90003d7e3427f262f430acd2403cab992807405f
atg0831 <atg0831@naver.com>
2020-01-09 14:15:15 +0900

    modify 2217

--- solution_atg/2217.py
+++ solution_atg/2217.py
@@ -1,5 +1,3 @@
-import sys
-
 N=int(input())
 list=[]
 ans=[]
@@ -9,15 +7,15 @@ for rope_weights in range(N):
 
 list.sort()
 list_len=len(list)
-list_max=list(list_len-1)
+list_max=max(list)
 ans.append(list_max)
 
 while list_len>1 :
+
     list_max=list[list_len-1]
     list_min=list[0]
     MAX=list_max
-    MIN=list_min * list_len  
-    
+    MIN=list_min * list_len
     if MAX>=MIN:
         ans.append(MAX)
 
@@ -26,17 +24,5 @@ while list_len>1 :
 
     list.pop(0)
     list_len-=1
-        # 4print(ans)
-    
 
 print(max(ans))
-
-
-
-
-
-# # 가장 작은 인자 뽑아내기 위해 len 사용
-# len_list=len(list)
-# print(len_list)
-
-# print(list[len_list-1])
